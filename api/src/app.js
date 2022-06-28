e87cde438585ea5b34baa4ffb0442f4530c94bd5
ValentinoMartz <valentinomartinez55@gmail.com>
2022-06-28 19:03:23 -0300

    string

--- api/src/app.js
+++ api/src/app.js
@@ -9,13 +9,13 @@ require("./db.js");
 const server = express();
 
 server.name = "API";
-
+let string = "*";
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", string); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
